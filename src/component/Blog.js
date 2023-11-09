ae599e04afd831e61c0d63664c66e432fb45ca13
mrkrezza1 <sajit1020@gmail.com>
2023-11-09 15:02:55 +0530

    her we have used the hook useref and then we have focus on the title bar if we submit the form

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 
 export default function Blog() {
 
@@ -6,12 +6,13 @@ export default function Blog() {
     // const [content,setContent] = useState("");
     const [formData, setFormData] = useState({ title: "", content: "" })
     const [blogs, setBlogs] = useState([]);
-
+    
+    const titleRef=useRef(null)
     function handleSubmit(e) {
         e.preventDefault();
-
         setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
         setFormData({ title: "", content: "" })
+        titleRef.current.focus()
         // console.log(blogs);
     }
 
@@ -30,6 +31,7 @@ export default function Blog() {
                         <input className="input"
                             value={formData.title}
                             placeholder="Enter the Title of the Blog here.."
+                            ref={titleRef}
                             onChange={(e) => setFormData({ title: e.target.value, content: formData.content })}
                         />
                     </Row >
@@ -42,7 +44,7 @@ export default function Blog() {
                         />
                     </Row >
 
-                    <button className="btn">ADD</button>
+                    <button style={{background:"green"}} className="btn">ADD</button>
                 </form>
 
             </div>
