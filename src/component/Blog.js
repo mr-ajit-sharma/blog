d5768b94a4ccecd784f3f02fae61db6441dbcd7b
mrkrezza1 <sajit1020@gmail.com>
2023-11-09 09:11:18 +0530

    here we are passing an object in usestate

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,67 +1,71 @@
 import { useState } from "react";
 
-export default function Blog(){
+export default function Blog() {
 
-    const [title,setTitle] = useState("");
-    const [content,setContent] = useState("");
-    const [blogs, setBlogs] =  useState([]);
+    // const [title,setTitle] = useState("");
+    // const [content,setContent] = useState("");
+    const [formData, setFormData] = useState({ title: "", content: "" })
+    const [blogs, setBlogs] = useState([]);
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
 
-        setBlogs([{title,content}, ...blogs]);
+        setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+        // setFormData({ title: "", content: "" })
         // console.log(blogs);
     }
 
-    return(
+    return (
         <>
-        <h1>Write a Blog!</h1>
-        <div className="section">
+            <h1>Write a Blog!</h1>
+            <div className="section">
 
-        {/* Form for to write the blog */}
-            <form onSubmit={handleSubmit}>
-                <Row label="Title">
+                {/* Form for to write the blog */}
+                <form onSubmit={handleSubmit}>
+                    <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."
-                                onChange = {(e) => setTitle(e.target.value)}
+                            value={formData.title}
+                            placeholder="Enter the Title of the Blog here.."
+                            onChange={(e) => setFormData({ title: e.target.value,content:formData.content})}
                         />
-                </Row >
+                    </Row >
 
-                <Row label="Content">
+                    <Row label="Content">
                         <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."
-                                onChange = {(e) => setContent(e.target.value)}
+                            value={formData.content}
+                            placeholder="Content of the Blog goes here.."
+                            onChange={(e) => setFormData({ title:formData.title,content: e.target.value })}
                         />
-                </Row >
-         
-                <button className = "btn">ADD</button>
-            </form>
-                     
-        </div>
+                    </Row >
 
-        <hr/>
+                    <button className="btn">ADD</button>
+                </form>
 
-        {/* Section where submitted blogs will be displayed */}
-        <h2> Blogs </h2>
-        {blogs.map((blog,i) => (
-            <div key={i} className="blog">
-                <h3>{blog.title}</h3>
-                <p>{blog.content}</p>
             </div>
-        ))}
-        
+
+            <hr />
+
+            {/* Section where submitted blogs will be displayed */}
+            <h2> Blogs </h2>
+            {blogs.map((blog, i) => (
+                <div key={i} className="blog">
+                    <h3>{blog.title}</h3>
+                    <p>{blog.content}</p>
+                </div>
+            ))}
+
         </>
-        )
-    }
+    )
+}
 
 //Row component to introduce a new row section in the form
-function Row(props){
-    const{label} = props;
-    return(
+function Row(props) {
+    const { label } = props;
+    return (
         <>
-        <label>{label}<br/></label>
-        {props.children}
-        <hr />
+            <label>{label}<br /></label>
+            {props.children}
+            <hr />
         </>
     )
 }
